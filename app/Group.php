a09980c7f1378b5e0435d48da2fb60ef1ba647da
Admin <01678424113a@gmail.com>
2017-09-21 10:26:28 +0700

    create model + controller + request for the group

--- app/Group.php
+++ app/Group.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    //
+    protected $table = 'groups';
+    public $timestamps = false;
+
+    public function category()
+    {
+        return $this->belongsTo(CategoriesPage::class,'id_category','id');
+    }
 }
--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\CategoriesPage;
+use App\Group;
 use Illuminate\Http\Request;
 
 class GroupController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = CategoriesPage::all();
+        $groups = Group::all();
+
+    }
 }
